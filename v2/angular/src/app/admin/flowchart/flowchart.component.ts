a54bb7de66e093d179b93392dbe0de876b39521a
Lupinheiro <40212718+lupinheiro@users.noreply.github.com>
2021-01-19 01:33:53 +0000

    Update flowchart.component.ts

---
+++ v2/angular/src/app/admin/flowchart/flowchart.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './flowchart.component.html',
+  styleUrls: ['./flowchart.component.css']
+})
+export class AppComponent {
+  title = 'bpmn-js-angular';
+  diagramUrl = 'https://github.com/lupinheiro/EindhovenClassificationMethod/blob/f92c159043523c2236197af67d57dfb0a002a68c/v2/angular/src/app/admin/flowchart/diagram-2.bpmn';
+  importError?: Error;
+
+  handleImported(event) {
+
+    const {
+      type,
+      error,
+      warnings
+    } = event;
+
+    if (type === 'success') {
+      console.log(`Rendered diagram (%s warnings)`, warnings.length);
+    }
+
+    if (type === 'error') {
+      console.error('Failed to render diagram', error);
+    }
+
+    this.importError = error;
+  }
+
+}
